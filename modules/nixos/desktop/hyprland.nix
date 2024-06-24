b219b4896c5fc4b2a022d93dca8dfd3b3814fa92
Eradax <erikadebahr@gmail.com>
2024-06-24 17:06:41 +0200

    test
    info:
      Profile: eradax
      Gen: 14 -> 15
      NixOs: 24.11.20240622.a71e967
      Kernel: 6.6.35

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -20,6 +20,6 @@ in {
     # environment.variables.NIXOS_OZONE_WL = "1";
 
     # enable hyprland and required options
-    programs.hyprland.enable = true;
+    # programs.hyprland.enable = true;
   };
 }
