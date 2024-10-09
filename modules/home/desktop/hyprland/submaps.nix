661fa2d42efd444d095b4f8e2501c8b39ac44dcd
Eradax <erikadebahr@gmail.com>
2024-10-09 12:00:59 +0200

    testing a feature
    info:
      Profile: eradax-laptop
      Gen: 58 -> 59
      NixOs: 24.11.20240919.c04d565
      Kernel: 6.6.52

--- modules/home/desktop/hyprland/submaps.nix
+++ modules/home/desktop/hyprland/submaps.nix
@@ -9,8 +9,8 @@
     binde = $mod, K, resizeactive, 0 -10
     binde = $mod, J, resizeactive, 0 10
 
-    bind = , escape, exec, truncate -s 0 /tmp/hypr_submap
-    bind = , escape, submap, reset
+    bind = $mod, R, exec, truncate -s 0 /tmp/hypr_submap
+    bind = $mod, R, submap, reset
     submap = reset
   '';
 }
