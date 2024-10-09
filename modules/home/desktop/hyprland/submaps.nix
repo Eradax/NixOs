afb79cc4d7019be31fcba88db43a49024c78f230
Eradax <erikadebahr@gmail.com>
2024-10-09 08:37:07 +0200

    Another fix?
    info:
      Profile: eradax-laptop
      Gen: 57 -> 58
      NixOs: 24.11.20240919.c04d565
      Kernel: 6.6.52

--- modules/home/desktop/hyprland/submaps.nix
+++ modules/home/desktop/hyprland/submaps.nix
@@ -1,11 +1,15 @@
 {...}: {
   wayland.windowManager.hyprland.extraConfig = ''
+    bind = $mod, R, exec, echo -n "Resize" > /tmp/hypr_submap
     bind = $mod, R, submap, resize
+
     submap = resize
     binde = $mod, H, resizeactive, -10 0
     binde = $mod, L, resizeactive, 10 0
     binde = $mod, K, resizeactive, 0 -10
     binde = $mod, J, resizeactive, 0 10
+
+    bind = , escape, exec, truncate -s 0 /tmp/hypr_submap
     bind = , escape, submap, reset
     submap = reset
   '';
