c7d2a0d32419997bcc2ce4c58e0b94bd2e5015bb
Eradax <erikadebahr@gmail.com>
2024-10-09 08:27:43 +0200

    Make it so you can leave the submap
    info:
      Profile: eradax-laptop
      Gen: 56 -> 57
      NixOs: 24.11.20240919.c04d565
      Kernel: 6.6.52

--- modules/home/desktop/hyprland/submaps.nix
+++ modules/home/desktop/hyprland/submaps.nix
@@ -6,5 +6,7 @@
     binde = $mod, L, resizeactive, 10 0
     binde = $mod, K, resizeactive, 0 -10
     binde = $mod, J, resizeactive, 0 10
+    bind = , escape, submap, reset
+    submap = reset
   '';
 }
