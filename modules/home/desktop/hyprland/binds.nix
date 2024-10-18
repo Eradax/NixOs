ca8272d660bf817227d1bfefeed9cf3511247bbc
Eradax <erikadebahr@gmail.com>
2024-10-19 00:51:19 +0200

    Another test
    info:
      Profile: eradax-laptop
      Gen: 80 -> 81
      NixOs: 24.11.20241009.5633bcf
      Kernel: 6.6.54

--- modules/home/desktop/hyprland/binds.nix
+++ modules/home/desktop/hyprland/binds.nix
@@ -80,7 +80,7 @@ in {
           imgDir = "~/screenshots";
         in
           mods: cmd: [
-            ''${mods}, PRINT, exec, mkdir -p ${imgDir}; grimblast --notify --freeze copysave ${cmd} "${imgDir}/${date}.png"''
+            ''${mods}, PRINT, exec, "mkdir -p ${imgDir}; grimblast --notify --freeze copysave ${cmd} ${imgDir}/${date}.png"''
           ];
       in
         [
